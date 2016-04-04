6892dd227b4fde7275aa8318aa6684763da2942d
divadgagnon <divadgagnon@hotmail.com>
2016-04-04 10:09:30 -0400

    Previous Commit Follow up
    Same as Previous

---
+++ Test_To_Delete/Views/MLTreturnPipeView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Media;
+using System.Windows.Controls;
+using GalaSoft.MvvmLight.Messaging;
+using LAB.Model;
+
+namespace LAB.ViewModel
+{
+
+    public partial class MLTreturnPipeView : UserControl
+    {
+        private SolidColorBrush WaterColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF1976CD"));
+        private SolidColorBrush TransparentBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0000"));
+
+        public MLTreturnPipeView()
+        {
+            InitializeComponent();
+            Messenger.Default.Register<Brewery.valve>(this, ValveUpdate_MessageReceived);
+        }
+
+        private void ValveUpdate_MessageReceived(Brewery.valve Valve)
+        {
+            if (Valve.Name != "MLTreturn") { return; }
+
+            if (Valve.IsOpen)
+            {
+                Water.Fill = WaterColor;
+            }
+            else
+            {
+                Water.Fill = TransparentBrush;
+            }
+        }
+    }
+}
---
+++ Test_To_Delete/Views/WaterManifoldView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using GalaSoft.MvvmLight.Messaging;
+using LAB.Model;
+
+namespace LAB.Views
+{
+
+    public partial class WaterManifoldView : UserControl
+    {
+        private SolidColorBrush WaterColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF1976CD"));
+        private SolidColorBrush TransparentBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0000"));
+
+        private Brewery.valve _MLTreturn;
+        private Brewery.valve _BKin;
+
+        public WaterManifoldView()
+        {
+            InitializeComponent();
+
+            _MLTreturn = new Brewery.valve();
+            _BKin = new Brewery.valve();
+
+            _MLTreturn.Name = "MLTreturn";
+            _BKin.Name = "BKin";
+
+            Messenger.Default.Register<Brewery.valve>(this, ValveUpdate_MessageReceived);
+        }
+
+        private void ValveUpdate_MessageReceived(Brewery.valve Valve)
+        {
+            if (Valve.Name != "MLTreturn" && Valve.Name != "BKin") { return; }
+
+            if (Valve.Name == "MLTreturn") { _MLTreturn.IsOpen = Valve.IsOpen; }
+            if(Valve.Name == "BKin") { _BKin.IsOpen = Valve.IsOpen; }
+
+            if (_BKin.IsOpen || _MLTreturn.IsOpen)
+            {
+                Water.Fill = WaterColor;
+            }
+            else
+            {
+                Water.Fill = TransparentBrush;
+            }
+        }
+    }
+}
